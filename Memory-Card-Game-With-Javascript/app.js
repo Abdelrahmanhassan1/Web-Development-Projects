25c033b72481ffd1e7b860e3b52c2fd61be6b5d4
abdelrahmanhassan1 <abdelrahmanwork1@gmail.com>
2021-10-04 21:54:03 +0200

    Adding the starting files

---
+++ Memory-Card-Game-With-Javascript/app.js
@@ -0,0 +1,5 @@
+const section = document.querySelector('section');
+const player_lives_count = document.querySelector('span');
+const player_lives = 6;
+
+player_lives_count.textContent = player_lives;
